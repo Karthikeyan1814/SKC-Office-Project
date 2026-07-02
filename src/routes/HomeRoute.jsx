3a39a46e3498aa559e86ea64edcd0ee1667520e9
Karthikeyan <km3974808@gmail.com>
2026-07-02 17:17:07 +0530

    signIn added

--- src/routes/HomeRoute.jsx
+++ src/routes/HomeRoute.jsx
@@ -2,10 +2,12 @@ import Nav from "../components/Nav"
 import Home from "../pages/Home"
 import Footer from "../components/Footer"
 import { Outlet } from "react-router-dom"
+import ThemeCustomizer from "../hook/ThemeCustomizer"
 export default function HomeRoute(){
     return(
         <div>
             <Nav />
+             <ThemeCustomizer />
            <Outlet />
             <Footer></Footer>
         </div>
